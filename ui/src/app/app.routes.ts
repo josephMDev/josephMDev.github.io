03f90340cc38a287a5e1248927e2077adb7e10fb
Joseph Moraru <19jam5@queensu.ca>
2025-01-04 13:34:59 -0500

    removed angular

--- ui/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CubeComponent } from './cube/cube.component';
-
-export const routes: Routes = [
-    {path: 'home', component: CubeComponent},
-    {path: '**', redirectTo: 'home'}
-];
